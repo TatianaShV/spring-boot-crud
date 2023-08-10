3b0a34e1957e4625e46d3ef9af3c6795bf13593f
TatianaShV <artemis1192@mail.ru>
2023-08-10 17:17:47 +0300

    init

--- src/main/java/springbootdemo/model/User.java
+++ src/main/java/springbootdemo/model/User.java
@@ -13,14 +13,35 @@ public class User {
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
-
+    @Column(name = "sex")
+    private String sex;
+    @Column(name = "age")
+    private int age;
     public User() {
     }
 
-    public User(Long id, String firstName, String lastName) {
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public User(Long id, String firstName, String lastName, String sex, int age) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.sex = sex;
+        this.age = age;
     }
 
     public Long getId() {
